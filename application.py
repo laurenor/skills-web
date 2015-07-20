53596601eed4c04d78c7e20bbe9a0894d86da1b2
Lauren <laurenortencio@gmail.com>
2015-07-19 22:18:05 -0700

    added some simple styling and worked on JS

--- application.py
+++ application.py
@@ -7,5 +7,19 @@ def index_page():
 
     return render_template("index.html")
 
+@app.route("/application-form")
+def appForm():
+
+
+
+@app.route("/application", methods=['POST'])
+def thanks_for_submission():
+	firstnm = request.form.get("firstname")
+    lastnm = request.form.get("lastname")
+    salaryamt = request.form.get("salary")
+    jobtitle = request.form.get("job")
+
+    return render_template("thanks.html", firstname = firstnm, lastname = lastnm, salary = salaryamt, job = jobtitle)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
