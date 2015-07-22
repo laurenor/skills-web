fd143a389583a7d9ec3d6b9a02b0c18a3c79e348
Lauren <laurenortencio@gmail.com>
2015-07-21 21:38:48 -0700

    edited code in flask to work with html files

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -8,18 +8,18 @@ def index_page():
     return render_template("index.html")
 
 @app.route("/application-form")
-def appForm():
-
+def app_form():
+	return render_template("application-form.html")
 
 
 @app.route("/application", methods=['POST'])
 def thanks_for_submission():
 	firstnm = request.form.get("firstname")
-    lastnm = request.form.get("lastname")
-    salaryamt = request.form.get("salary")
-    jobtitle = request.form.get("job")
+	lastnm = request.form.get("lastname")
+	salaryamt = request.form.get("salary")
+	jobtitle = request.form.get("job")
 
-    return render_template("thanks.html", firstname = firstnm, lastname = lastnm, salary = salaryamt, job = jobtitle)
+	return render_template("thanks.html", firstname = firstnm, lastname = lastnm, salary = salaryamt, job = jobtitle)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
